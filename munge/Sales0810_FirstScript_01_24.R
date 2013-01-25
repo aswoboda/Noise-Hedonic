e04d1c101afb610ea663896d94578d3c7cff939f
aswoboda <aaron.swoboda@gmail.com>
2013-01-25 15:53:17 -0600

    added a few helpful commands for future reference

--- munge/Sales0810_FirstScript_01_24.R
+++ munge/Sales0810_FirstScript_01_24.R
@@ -1,3 +1,21 @@
 library("foreign")
 sales0810 <- read.dbf("../Data/Sales20082010.dbf")
 summary(sales0810)
+str(sales0810)
+
+# here's some code about how to eliminate variables we don't want...
+dataNames = names(sales0810)
+varsIdontWant = which(dataNames %in% c("PIN", "BLDG_NUM", "X7_MCA"))
+
+sales0810[1:5, -varsIdontWant]
+
+
+names(sales0810)
+
+model1 = lm(EMV_TOTAL ~ HOME_STYLE + SALE_YR, data = sales0810)
+
+names(model1)
+
+ResidualsDF = data.frame(PIN = sales0810$PIN, model1$residuals)
+
+write.dbf(ResidualsDF, "../Data/toArcMap/example.dbf" )
