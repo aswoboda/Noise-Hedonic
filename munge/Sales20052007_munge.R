23d753e6e48494d44fa8c525806817efff613f6c
timmm <timmm@carleton.edu>
2013-01-29 10:18:52 -0600

    Moved analysis and diagnostics to a new script entitled MaxSandbox[YEARS]. Created a new folder within Data/CleanData that stores the cleaned .dbf tables from the raw data taken from GIS.

--- munge/Sales20052007_munge.R
+++ munge/Sales20052007_munge.R
@@ -49,61 +49,6 @@ testcor <- which(dataNames %in% c("ACRES_POLY", "FIN_SQ_FT", "YEAR_BUILT", "SALE
 testcor1 = workingdata[ , testcor]
 cor(testcor1) 
 
-###Preparation for regression analysis, load packages for tests
-install.packages ("zoo")
-install.packages ("lmtest")
-install.packages ("car")
-require(zoo)
-require(lmtest)
-require(car)
-
-
-
-####Linear model investigation
-##Dependent variable: Sales Value (untransformed)
-model.SaleValue1 <- lm (SALE_VALUE ~ CITY + SALE_YR + SDNUM + ACRES_POLY+HOMESTEAD+FIN_SQ_FT+YEAR_BUILT+MAX+PARK_dist+LAKE_dist+MCA3+MCA5+SHOP_dist+CBD_dist, data=workingdata)
-summary(model.SaleValue1)
-plot(model.SaleValue1) #Normality issues once again #16300 17135 large leverage
-anova(model.SaleValue1)
-
-workingdata = workingdata[-c(16300, 17135), ] #Removed influential observations
-
-##Dependent variable: Sales Value (untransformed) w/o MCA5
-model.SaleValue2 <- lm (SALE_VALUE ~ CITY + SALE_YR + SDNUM + ACRES_POLY+HOMESTEAD+FIN_SQ_FT+YEAR_BUILT+MAX+PARK_dist+LAKE_dist+MCA3+SHOP_dist+CBD_dist, data=workingdata)
-summary(model.SaleValue2)
-plot(model.SaleValue2) #Normality issues once again 
-anova(model.SaleValue2, model.SaleValue1)
-
-##Dependent variable: Sales Value (untransformed) w/o MCA5, PARK_dist, SHOP_dist, SDnum
-model.SaleValue3 <- lm (SALE_VALUE ~ CITY + SALE_YR  + ACRES_POLY+HOMESTEAD+FIN_SQ_FT+YEAR_BUILT+MAX+LAKE_dist+MCA3+CBD_dist, data=workingdata)
-summary(model.SaleValue3)
-plot(model.SaleValue3) #Normality issues once again
-anova(model.SaleValue2, model.SaleValue3) #Comparing Anova values suggests to keep the smaller model.
-vif(model.SaleValue3) #multicollinearity #No real signs of severe multicollinearity
-dwtest (model.SaleValue3) #autocorrelation # Signs of autocorrelation since DW value is less than 1.5 (1.4266)
-bptest(model.SaleValue3) #shows some signs of heteroscedasticity
-
-#Dependent variable: Sales Value (untransformed) w/o MCA5, PARK_dist, SHOP_dist, SDNUM | inclusion of Acres2 
-model.SaleValue4 <- lm (SALE_VALUE ~ CITY + SALE_YR  + ACRES_POLY+ ACRES2 +HOMESTEAD+FIN_SQ_FT+YEAR_BUILT+MAX+LAKE_dist+MCA3+CBD_dist, data=workingdata)
-summary(model.SaleValue4)
-plot(model.SaleValue4) #Normality issues once again
-#Diagnostics
-outlierTest(model.SaleValue4)
-ncvTest(model.SaleValue4) #nonconstant variance
-dwtest (model.SaleValue4) #autocorrelation # Signs of autocorrelation since DW value is less than 1.5 (1.4266)
-bptest(model.SaleValue4)
-
-
-
-
-######Normality Issues arise so we investigate with the log transformation on the dependent variable
-##Dependent variable: Sales Value (log transformed) w/o MCA5, PARK_dist, SHOP_dist SDNUM | inclusion of Acres2, logMax, logFIN_SQ_FT
-model.logSaleValue1 <- lm (logSALE_VALUE ~ CITY + SALE_YR + ACRES_POLY+ACRES2+HOMESTEAD+ logFIN_SQ_FT + YEAR_BUILT+logMAX+LAKE_dist+MCA3+SHOP_dist+CBD_dist, data=workingdata)
-summary(model.logSaleValue1) #Explains significantly less variation than untransformed model
-plot(model.logSaleValue1)
-anova(model.logSaleValue1)
-#Diagnostics
-outlierTest(model.logSaleValue1)
-ncvTest(model.logSaleValue1) #nonconstant variance does not improve
-dwtest (model.logSaleValue1) #autocorrelation
-bptest(model.logSaleValue1)
+##Write new .dbf table and store it in 'CleanData' folder
+Sales20052007Clean = data.frame(workingdata)
+write.dbf(Sales20052007Clean, "../Data/CleanData/Sales20052007.dbf" )
