b9044c9d9845a96a4e712732ca1933d1a842e42e
aswoboda <aaron.swoboda@gmail.com>
2013-03-25 12:50:52 -0500

    added "mc.cores" option to limit the number of cores used

--- analysis/04MonteCarloSim/MonteCarloScript2AssesSpatialNonStationarity.R
+++ analysis/04MonteCarloSim/MonteCarloScript2AssesSpatialNonStationarity.R
@@ -40,7 +40,8 @@ for (iter in 1:iterations) {
                       Data.Frame = simDATA,
                       my.model = MYMODEL, my.modelSMALL = MYMODELsmall,
                       kvector = KVECTOR,
-                        timelag = 12
+                      timelag = 12,
+                        mc.cores = 12
   )
   names(output.raw) = simDATA$UNIQID[obs2run]
   output = Reorganizer(output.raw)
