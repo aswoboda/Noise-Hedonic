b89f09b996aa0eb33a848872a562d1bf8de74416
aswoboda <aaron.swoboda@gmail.com>
2013-04-27 15:05:49 -0500

    created and populated new folder containing MC sim with city fixed effects

---
+++ analysis/04MonteCarloSim/ModelBigCity/MonteCarloScript2AssesSpatialNonStationarity.R
@@ -0,0 +1,70 @@
+# Run a Monte Carlo experiment to see whether coefficients really vary over space
+
+require(foreign)
+require(multicore)
+require(fields, quietly = TRUE)
+# the following command loads up some functions we'll use
+source("helper/LWRfunctions.R")
+
+myVars = c("MAX", "FIN_SQ_FT", "ACRES_POLY", "YEAR_BUILT", "OWNOCC", "MED_INCOME", "MCA3",
+           "LAKE_dist", "PARK_dist", "SHOP_dist", "CBD_dist", "HOME_STYLE", "CITY")
+RHS = paste(myVars, collapse = "+")
+MYMODEL = paste("logSALE_VA", RHS, sep = "~")
+MYMODELsmall = substr(MYMODEL, 1, nchar(MYMODEL)-5)
+KVECTOR = c(50, 100, 175, 200, 225, 250, 300, 400, 600, 1000, 2000, 4000, 10000)
+
+# How many times am I going to reshuffle?
+iterations = 2
+# How many things am I keeping track of each reshuffle? 
+# mean and sd of each coefficient i care about + intercept + GCV score + min bandwidth
+vars2keep = c("Intercept", myVars[c(1:(length(myVars)-2))])
+numMCstats = 2 + 2*length(vars2keep)
+MCstats = matrix(NA, iterations, numMCstats)
+colnames(MCstats) = c("minGCV", "optimalBandwidth", 
+                      paste0("meanBeta.", vars2keep),
+                      paste0("sterBeta.", vars2keep))
+
+filePrefix = "../Data/R2GIS/CleanData/"
+inputFile = "Sales20052010.dbf"
+DATAFRAME = read.dbf(paste0(filePrefix, inputFile))
+simDATA = DATAFRAME
+N = dim(simDATA)[1]
+obs2run = which(simDATA$TimePeriod>11)
+for (iter in 1:iterations) {
+  # Do a reshuffle
+  rowShuffle = sample(1:N)
+  simDATA[, c("Long_X", "Lat_Y")] = DATAFRAME[rowShuffle, c("Long_X", "Lat_Y")]
+  start = Sys.time()
+  
+  output.raw = mclapply(obs2run,
+                      LWRtimelag,
+                      Data.Frame = simDATA,
+                      my.model = MYMODEL, my.modelSMALL = MYMODELsmall,
+                      kvector = KVECTOR,
+                      timelag = 12,
+                        mc.cores = 16
+  )
+  names(output.raw) = simDATA$UNIQID[obs2run]
+  output = Reorganizer(output.raw)
+  
+  gcvs = GCV(leverages = output$leverages, 
+             yhats = output$yhats, 
+             dep.var = simDATA[obs2run, "logSALE_VA"])
+  minGCVnumber = which.min(gcvs)
+  minGCV = gcvs[minGCVnumber]
+  optimalBandwidth = KVECTOR[minGCVnumber]
+  
+  MCstats[iter, "minGCV"] = minGCV
+  MCstats[iter, "optimalBandwidth"] = optimalBandwidth
+  for (i in 1:length(vars2keep)) {
+    MCstats[iter, i+2] = mean(output[[i]][, minGCVnumber])
+    MCstats[iter, i+2+length(vars2keep)] = sd(output[[i]][, minGCVnumber])
+  }
+  end = print(Sys.time())
+  print(paste("iteration ", iter, " took "))
+  print(end - start)
+  write.csv(MCstats, file = paste0("~/NoiseHedonicProject/Noise-Hedonic/analysis/04MonteCarloSim/ModelBigCity/LWRMonteCarloStats", Sys.Date(), ".csv"), row.names = FALSE)
+  rm(output, output.raw)
+  gc()
+  print(gc())
+}
