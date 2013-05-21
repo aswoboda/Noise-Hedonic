928d94e41c5d7336fa91d1a2baf6ca6a378e5db2
aswoboda <aaron.swoboda@gmail.com>
2013-05-21 17:13:07 -0500

    updated some file locations

--- analysis/04MonteCarloSim/ModelBigCity/MCsimVisualizer.R
+++ analysis/04MonteCarloSim/ModelBigCity/MCsimVisualizer.R
@@ -6,13 +6,12 @@
 # load the monte carlo data
 # create distributions of the MC stats
 # add the true statistic to the distribution
+setwd("~/NoiseHedonicProject/Noise-Hedonic/analysis/04MonteCarloSim/ModelBigCity/")
+load("CopyOfSales20052010LWRmodel18-2013-04-13.RData") # LWR output from model run with real data
+MCMaster = read.csv("bandwidthsAll/LWRMonteCarloStats2013-05-06.csv") # Monte Carlo stats from all bandwidths
+MCMaster200 = read.csv("bandwidth200/LWRMonteCarloStats2013-05-10.csv") # Monte Carlo stats from nearest 200 obs
 
-load("../Data/R2GIS/CleanData/Sales20052010LWRmodel18-2013-04-13.RData") # LWR output from model run with real data
-MCMaster = read.csv("analysis/04MonteCarloSim/ModelBigCity/bandwidthsAll/LWRMonteCarloStats2013-05-06.csv") # Monte Carlo stats from all bandwidths
-MCMaster200 = read.csv("analysis/04MonteCarloSim/ModelBigCity/bandwidth200/LWRMonteCarloStats2013-05-10.csv") # Monte Carlo stats from nearest 200 obs
-
-
-summary(MCMaster200)
+summary(MCMaster)
 
 MCplotter = function(outputcoefficient, statcoefficient, meanORsd = "mean") {
   if (meanORsd == "mean") actual = mean(output[[outputcoefficient]][, "k200"], na.rm =T)
@@ -31,7 +30,7 @@ outputCOEFS = rep(names(output)[c(1:5, 20:26)], 2)
 statCOEFS = names(MCMaster200)[3:26]
 meanORsds = rep(c("mean", "sd"), c(12, 12))
 
-pdf("analysis/04MonteCarloSim/ModelBigCity/MCsimResults.pdf", height = 6, width = 24)
+pdf("MCsimResults.pdf", height = 6, width = 24)
 par(mfrow = c(2, 12))
 for (i in 1:length(outputCOEFS)) {
   MCplotter(outputCOEFS[i], statCOEFS[i], meanORsds[i])
