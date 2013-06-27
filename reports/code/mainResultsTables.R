4408570aab89c977692efe3510ff0c1bab950b25
aswoboda <aaron.swoboda@gmail.com>
2013-06-27 11:10:42 -0500

    cleaned up the main results table

--- reports/code/mainResultsTables.R
+++ reports/code/mainResultsTables.R
@@ -24,33 +24,42 @@ lm.global = lm(paste0(MYMODEL, "+factor(SALE_YR)"), data = DATAFRAME[obs2run, ])
 GlobalModel = summary(lm.global)
 global.leverages = lm.influence(lm.global, do.coef = FALSE)$hat
 
-GCV(leverages = matrix(global.leverages, ncol = 1), 
-    yhats = matrix(lm.global$fitted.values, ncol = 1), 
-    dep.var = DATAFRAME$logSALE_VA[obs2run])
-
 vars = c("(Intercept)", "MAX", "FIN_SQ_FT", "ACRES_POLY", "YEAR_BUILT", "OWNOCC", 
          "MCA3", "MED_INCOME", "CBD_dist", "PARK_dist", "LAKE_dist", "SHOP_dist" )
 
-LWRtable = matrix(NA, length(vars), 6)
-rownames(LWRtable) = vars
-LWRtable[, 1:2] = signif(GlobalModel$coefficients[vars , 1:2], 2)
+numvars = length(vars)
+LWRtable = matrix(NA, numvars+2, 6)
+rownames(LWRtable) = c("(Intercept)", "Traffic Noise", "House Size", "Lot Size", "Year of Construction", 
+                       "Owner Occupancy", "Elementary Test Scores", "Median Income", 
+                       "distance to CBD", "distance to nearest Park", "distance to nearest Lake", 
+                       "dist. to nearest Shopping Center", "GCV score")
+LWRtable[1:numvars, 1:2] = signif(GlobalModel$coefficients[vars , 1:2], 2)
+
+LWRtable[numvars + 1, 1] = GCV(leverages = matrix(global.leverages, ncol = 1), 
+                               yhats = matrix(lm.global$fitted.values, ncol = 1), 
+                               dep.var = DATAFRAME$logSALE_VA[obs2run])
+
+LWRtable[numvars + 1, 3] = min(GCV(output$leverages, output$yhats, DATAFRAME$logSALE_VA[obs2run]))
+
+LWRtable[numvars + 2, 1] = signif(GlobalModel$r.squared, 2)
+
 
 # populate the rest of the table
-for (i in 1:length(vars)) {
+for (i in 1:numvars) {
   # put the mean and sd of LWR beta hats in the table
   LWRtable[i, 3] = signif(mean(output[[paste0("beta.", vars[i])]][, "k200"], na.rm = T), 2)
-  LWRtable[i, 5] = signif( sd(output[[paste0("beta.", vars[i])]][, "k200"], na.rm = T), 2)
+  LWRtable[i, 4] = signif( sd(output[[paste0("beta.", vars[i])]][, "k200"], na.rm = T), 2)
   
   # what percent of the coefficients are significantly different from zero at 10% level?
   tstats = output[[paste0("beta.", vars[i])]][, "k200"]/output[[paste0("ses.", vars[i])]][, "k200"]
-  LWRtable[i, 4] = signif(length(which(abs(tstats) > qnorm(.95)))/sum(!is.na(tstats)), 2)
+  LWRtable[i, 5] = signif(length(which(abs(tstats) > qnorm(.95)))/sum(!is.na(tstats)), 2)
   
   # add the Monte Carlo p-value
   # pick a variable
   # grab the sd of the LWR betahats
   # grab the 100 sds from the Monte Carlo simulations for the variable
   # rank the real LWR betahat sd within the Monte Carlo simulation values
-  mysd = LWRtable[i, 5]
+  mysd = LWRtable[i, 4]
   varid = sub("\\(", "", vars[i])
   varid = sub("\\)", "", varid)
   othersds = LWRMCstats[, paste0("sterBeta.", varid)]
@@ -60,7 +69,7 @@ for (i in 1:length(vars)) {
 LWRtable
 require(xtable)
 colnames(LWRtable) = paste0("(", 1:6, ")")
-xtable(LWRtable, digits = c(0, -1, -1, -1, 2, -1, 2))
+xtable(LWRtable, digits = c(0, -1, -1, -1, -1, 2, 2))
 
 
 ###################
