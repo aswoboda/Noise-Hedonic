5a1e2b89a44734de3d624a12467cb107210584ea
aswoboda <aaron.swoboda@gmail.com>
2013-09-30 15:27:53 -0500

    another round of editing, updated correlation matrix, etc.

--- analysis/Data Description/CorMatrix.R
+++ analysis/Data Description/CorMatrix.R
@@ -3,7 +3,7 @@ library (foreign)
 workingdata <- read.dbf("../Data/R2GIS/CleanData/Sales20052010.dbf")
 
 
-quantVars = c("logSALE_VA", "FIN_SQ_FT", "ACRES_POLY", "YEAR_BUILT", 
+quantVars = c("SALE_VALUE", "FIN_SQ_FT", "ACRES_POLY", "YEAR_BUILT", 
               "MAX", "MED_INCOME", "MCA3", "CBD_dist", "PARK_dist", "LAKE_dist",  "SHOP_dist")
 
 corMat = cor(workingdata[, quantVars])
@@ -13,7 +13,7 @@ corMat = round(corMat, 2)
 corMat[upper.tri(corMat)] <- ""
 corMat
 
-rownames(corMat) = c("ln Sale Price", "House Size", "Lot Size", "Year Built", "Noise", "Income", "Elementary Test Scores",
+rownames(corMat) = c("Sale Price", "House Size", "Lot Size", "Year Built", "Noise", "Income", "Elementary Test Scores",
                      "Distance to CBD", "Distance to Park", "Distance to Lake", "Distance to Shop")
 colnames(corMat) = c("Price", "House", "Lot", "Built", "Noise", "Income", "Test", "CBD", "Park", "Lake", "Shop")
 
